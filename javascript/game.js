a113a96f56690f225237d283d1b73e272394b9fe
Rukaroa <rukaroa@gmail.com>
2017-02-05 18:00:16 -0600

    initial variables

---
+++ javascript/game.js
@@ -0,0 +1,43 @@
+/*Choose a theme for your game! In the demo, we picked an 80s theme: 80s questions, 80s sound and an 80s aesthetic. You can choose any subject for your theme, though, so be creative!
+
+Use key events to listen for the letters that your players will type.
+
+Display the following on the page:
+
+    Press any key to get started!
+
+    Wins: (# of times user guessed the word correctly).
+
+        If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
+
+        As the user guesses the correct letters, reveal them: m a d o _ _ a.
+
+    Number of Guesses Remaining: (# of guesses remaining for the user).
+
+    Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+
+After the user wins/loses the game should automatically choose another word and make the user play it.
+*/
+
+//youkai theme
+
+//Start Game
+
+//pick random word
+
+function randomWord(wordPool){
+	return wordPool[Math.floor(Math.random() * wordPool.length)];
+}
+//display random word as boxes or blanks
+
+//min guesses = 5; max guesses = word length / 2 + 5
+
+//display letter in place of box/blank on correct guess
+
+//keep track of wins and losses
+
+//loop game until exited(?)
+
+//show pictures on win/lose
+
+//initialize game variables
